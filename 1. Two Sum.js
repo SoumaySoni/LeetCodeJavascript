8b7f3d265623d40d2ccc29cc44e2f3976ac8f834
soumay soni <soumaysoni1687@gmail.com>
2026-01-15 01:01:50 +0530

    Two sum

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -4,14 +4,40 @@
 
 // You can return the answer in any order.
 
-var twoSum = function(nums, target) {
-    for(var i=0;i<=nums.length;i++){
-        for(var j=i+1;j<=nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return [i,j]
+// Brute Force
+
+// Time Complexity: O(n²)
+// Space Complexity: O(1)
+
+var twoSum = function (nums, target) {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
             }
         }
     }
 };
 
-console.log(twoSum([1, 2, 3, 4, 5], 6));
+console.log(twoSum([1, 2, 3, 4, 5], 6)); // [0, 4] or [1, 3]
+
+// Optimized Approach using Hash Map
+
+// Time: O(n)
+// Space: O(n)
+
+var twoSum = function (nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+};
+
+console.log(twoSum([1, 2, 3, 4, 5], 6)); // [0, 4] or [1, 3]
