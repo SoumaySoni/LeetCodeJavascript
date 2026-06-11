5b02fb32e995eb01057147ca8aceac3eb0347b38
Soumay Soni <soumaysoni1687@gmail.com>
2026-06-12 02:17:40 +0530

    add 118. Pascal's Triangle.js

---
+++ 118. Pascal's Triangle.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    let result = [];
+
+    for (let i = 0; i < numRows; i++) {
+        let row = [];
+
+        for (let j = 0; j <= i; j++) {
+            // First and last elements are always 1
+            if (j === 0 || j === i) {
+                row.push(1);
+            } else {
+                // Sum of the two numbers directly above
+                row.push(result[i - 1][j - 1] + result[i - 1][j]);
+            }
+        }
+
+        result.push(row);
+    }
+
+    return result;
+};
+
+console.log(generate(5));
