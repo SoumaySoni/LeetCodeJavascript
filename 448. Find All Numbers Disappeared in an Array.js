6d269b4c1118f64800811ae1f7d28b59087ddb4e
Soumay Soni <soumaysoni1687@gmail.com>
2026-06-26 22:23:51 +0530

    448. Find All Numbers Disappeared in an Array.js

---
+++ 448. Find All Numbers Disappeared in an Array.js
@@ -0,0 +1,17 @@
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    var findDisappearedNumbers = function(nums) {
+        for(let i = 0; i < nums.length; i++){
+            const index = Math.abs(nums[i]) - 1;
+            nums[index] = -Math.abs(nums[index])
+        }
+        const result = [];
+        for(let i=0; i<nums.length;i++){
+            if(nums[i]>0) result.push(i+1)
+        }
+        return result;
+    };
+
+    console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
