fe2a3cdc0d72381eb3677c7dc02aadf7ddd52fe9
soumay soni <154653242+SoumaySoni@users.noreply.github.com>
2026-07-31 02:22:36 +0530

    Time: 0 ms (100%), Space: 51.7 MB (99.31%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let i = s.length - 1;
+
+    // Skip trailing spaces
+    while (i >= 0 && s[i] === " ") {
+        i--;
+    }
+
+    let length = 0;
+
+    // Count the last word
+    while (i >= 0 && s[i] !== " ") {
+        length++;
+        i--;
+    }
+
+    return length;
+};
